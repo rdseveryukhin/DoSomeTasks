badabdc550fe0276f10e13dbe0588831c02060f0
Roman <122228334+rdseveryukhin@users.noreply.github.com>
2023-02-26 19:35:35 +0400

    Update README.md

---
+++ BracersSequence/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,62 @@
+﻿#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ifstream file("C:\\Users\\rdsev\\Desktop\\data.txt");
+	ofstream fout("C:\\Users\\rdsev\\Desktop\\out.txt");
+	char inChar = '\0';
+	bool isEqual = true;
+	bool isInBody = false;
+	int countBreacketOpen = 0;
+	int countBreacketClose = 0;
+
+
+	while (!file.eof()) {
+		
+		file >> inChar;
+		if (file.eof()) {
+			break;
+		}
+
+		if (!isInBody) {
+			if (inChar == '}') {		//if body closes without opening before
+				isEqual = false;
+				break;
+			}
+			else if (inChar == '{') {	//if body opens
+				isInBody = true;
+				countBreacketOpen++;
+			}
+		}
+		else {
+			if (inChar == '{') {		//if one more body opens
+				countBreacketOpen++;
+			}
+			else if (inChar == '}') {	//if body closes
+				countBreacketClose++;
+				if (countBreacketOpen == countBreacketClose) {	//if all bodies close
+					isInBody = false;
+					countBreacketOpen = 0;
+					countBreacketClose = 0;
+				}
+
+			}
+		}
+
+	}
+	if (countBreacketOpen != countBreacketClose) {
+		isEqual = false;
+	}
+
+	//output
+	if (isEqual) {
+		cout << "Breackets are matched";
+		fout << "Breackets are matched";
+	}
+	else {
+		cout << "Breackets AREN'T matched";
+		fout << "Breackets AREN'T matched";
+	}
+}
