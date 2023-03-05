d4802040c09d6e5f0a1e519bff51c82d7826ad0d
Roman <122228334+rdseveryukhin@users.noreply.github.com>
2023-03-05 22:51:20 +0400

    Update README.mdCreate README.md

---
+++ MakeIntervals/MakeIntervals.cpp
@@ -0,0 +1,27 @@
+﻿#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string makeIntervals(vector<int>& arr) {
+	string result = "";
+	if (!arr.empty()) {
+		sort(arr.begin(), arr.end());
+		for (int i = 0; i + 1 < arr.size(); i++) {
+			if (arr[i + 1] - arr[i] == 1 && (result == "" || result[result.length() - 1] != '-')) {
+				result += to_string(arr[i]) + "-";
+			}
+			else if (arr[i + 1] - arr[i] > 1) {
+				result += to_string(arr[i]) + ",";
+			}
+		}
+		result += to_string(arr[arr.size() - 1]);
+	}
+	return result;
+}
+
+int main() {
+	vector<int> a = { 1,4,7,8,10,3,2 };
+	cout << makeIntervals(a);
+}
